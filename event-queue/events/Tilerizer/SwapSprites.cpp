bf27925bb2f2ac2017c560e978694763df61441d
Ukkiez <yannick_3d@hotmail.com>
2023-01-24 23:39:51 +0100

    have Beautify do more, across multiple layers, and don't change dustblocks to solid blocks by accident

--- event-queue/events/Tilerizer/SwapSprites.cpp
+++ event-queue/events/Tilerizer/SwapSprites.cpp
@@ -7,11 +7,14 @@
 
 class SwapSprites : QueueEvent {
   QueueEventConfig get_config() {
-    return QueueEventConfig( 800, 0, "Exec Func Ruin Level", "Beautify" );
+    return QueueEventConfig( 800, 120, "Exec Func Ruin Level", "Beautify" );
   }
 
   scene@ g;
   dustman@ player;
+
+  uint frames = 0;
+  uint interval = 10;
 
   bool initialized = false;
 
@@ -19,8 +22,65 @@ class SwapSprites : QueueEvent {
     @g = get_scene();
   }
 
-  void step( int entities ) {}
-  void draw( float sub_frame ) {}
+  void step( int entities ) {
+    if ( frames % interval == 0 ) {
+      if ( frames < 30 ) {
+        replace_tiles( 19 );
+      }
+      else if ( frames < 40 ) {
+        replace_tiles( 15 );
+      }
+      else if ( frames < 50 ) {
+        replace_tiles( 14 );
+      }
+      else if ( frames < 60 ) {
+        replace_tiles( 13 );
+      }
+      else if ( frames < 70 ) {
+        replace_tiles( 11 );
+      }
+      else if ( frames < 80 ) {
+        replace_tiles( 10 );
+      }
+      else if ( frames < 90 ) {
+        replace_tiles( 9 );
+      }
+      else if ( frames < 100 ) {
+        replace_tiles( 8 );
+      }
+      else if ( frames < 110 ) {
+        replace_tiles( 7 );
+      }
+      else if ( frames <= 120 ) {
+        replace_tiles( 6 );
+      }
+    }
+
+    frames++;
+  }
+
+  void replace_tiles( uint layer ) {
+    uint sprite_set = srand_range( 1, 4 );
+    uint sprite_tile = srand_range( 1, 5 );
+
+    for ( uint i = 0; i <= 50; i++ ) {
+      int x = tile_coord( player.x() + tile( srand_range( -60, 60 ) ) );
+      int y = tile_coord( player.y() + tile( srand_range( -20, 20 ) ) );
+
+      // attempt to create "bowl" shapes in random places
+      replace_tile( g, x, y, sprite_set, sprite_tile, -1, layer );
+      replace_tile( g, x+1, y, sprite_set, sprite_tile, -1, layer );
+      replace_tile( g, x+2, y, sprite_set, sprite_tile, -1, layer );
+      replace_tile( g, x-1, y, sprite_set, sprite_tile, -1, layer );
+      replace_tile( g, x-2, y, sprite_set, sprite_tile, -1, layer );
+
+      replace_tile( g, x+1, y+1, sprite_set, sprite_tile, -1, layer );
+      replace_tile( g, x-1, y+1, sprite_set, sprite_tile, -1, layer );
+
+      replace_tile( g, x, y+1, sprite_set, sprite_tile, -1, layer );
+      replace_tile( g, x, y+2, sprite_set, sprite_tile, -1, layer );
+    }
+  }
 
   void initialize() {
     if ( initialized ) {
@@ -30,27 +90,6 @@ class SwapSprites : QueueEvent {
     controllable@ c = controller_controllable( 0 );
     @player = c.as_dustman();
 
-    uint sprite_set = srand_range( 1, 4 );
-    uint sprite_tile = srand_range( 1, 5 );
-
-    for ( uint i = 0; i <= 50; i++ ) {
-      int x = tile_coord( player.x() + tile( srand_range( -60, 60 ) ) );
-      int y = tile_coord( player.y() + tile( srand_range( -20, 20 ) ) );
-
-      // attempt to create "bowl" shapes in random places
-      replace_tile( g, x, y, sprite_set, sprite_tile );
-      replace_tile( g, x+1, y, sprite_set, sprite_tile );
-      replace_tile( g, x+2, y, sprite_set, sprite_tile );
-      replace_tile( g, x-1, y, sprite_set, sprite_tile );
-      replace_tile( g, x-2, y, sprite_set, sprite_tile );
-
-      replace_tile( g, x+1, y+1, sprite_set, sprite_tile );
-      replace_tile( g, x-1, y+1, sprite_set, sprite_tile );
-
-      replace_tile( g, x, y+1, sprite_set, sprite_tile );
-      replace_tile( g, x, y+2, sprite_set, sprite_tile );
-    }
-
     initialized = true;
   }
 
@@ -58,6 +97,8 @@ class SwapSprites : QueueEvent {
     if ( !initialized ) {
       return;
     }
+
+    frames = 0;
 
     initialized = false;
   }
--- lib/util/tiles.cpp
+++ lib/util/tiles.cpp
@@ -15,9 +15,9 @@ int center_coord_on_tile( int v ) {
 
 // replace a tile, not setting a new one in empty space
 void replace_tile( scene@ g, int x, int y, int sprite_set, int sprite_tile = -1, int type = -1, uint layer = 19, uint palette = 1 ) {
-  tileinfo@ ti = g.get_tile( x, y );
+  tileinfo@ ti = g.get_tile( x, y, layer );
 
-  if ( ti.solid() ) {
+  if ( ti.solid() && !ti.is_dustblock() ) {
     g.set_tile(
       x,
       y,
